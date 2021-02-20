6623e86976fada0de4306cc3737e5b93f63169d0
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-19 21:50:57 -0300

    feat(pages): Show ProfileSection on Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,46 @@
-import styled from 'styled-components'
+import { useEffect, useState } from 'react'
+import { ProfileProps } from '../src/components/Profile'
+import ProfileSection from '../src/components/ProfileSection'
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+const Home = () => {
+  const [characters, setCharacters] = useState<ProfileProps[]>([])
 
-export default function Home() {
-  return <Title>My page</Title>
+  const parseCharacters = (characters: any[]): ProfileProps[] => {
+    return characters.map(character => ({
+      image: character.image,
+      name: character.name,
+      status: character.status,
+      species: character.species,
+      location: character.location.name,
+      origin: character.origin.name,
+      number_of_episodes: character.episode.length
+    }))
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('https://rickandmortyapi.com/api/character')
+      console.log(response)
+      const data = await response.json()
+      console.log(data)
+      const parsedCharacters = parseCharacters(data.results)
+      console.log(parsedCharacters.slice(0, 5))
+      setCharacters(parsedCharacters)
+    }
+
+    getData()
+  }, [])
+
+  return (
+    <>
+    {
+      (characters.length === 0) ? <h2>Loading</h2>
+      : (
+        <ProfileSection characters={characters} />        
+      )
+    }
+    </>
+  )
 }
+
+export default Home
