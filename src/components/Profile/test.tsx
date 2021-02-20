62bd5fc478158b7909f1f66e7e7f337c40738266
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-20 11:14:57 -0300

    chore(Profile): Remove unecessary test

--- src/components/Profile/test.tsx
+++ src/components/Profile/test.tsx
@@ -20,26 +20,4 @@ describe('<Profile />', () => {
         const episodesText = `Episodes it appeared in: ${props.numberOfEpisodes}`
         expect(screen.getByText(new RegExp(episodesText, "i"))).toBeInTheDocument()
     })
-
-    it('should render the status circle color accordingly', () => {
-        const props = Default.args as ProfileProps
-        const aliveArgs = { ...props, status: 'Alive' as ProfileProps['status'] }
-        const deadArgs = { ...props, status: 'Dead' as ProfileProps['status'] }
-        const unknownArgs = { ...props, status: 'unknown' as ProfileProps['status'] }
-        
-        const {rerender } = render(<Default {...aliveArgs} />)
-        expect(screen.getByTestId('status')).toHaveStyle({
-            backgroundColor: 'green'
-        })
-        
-        rerender(<Default {...deadArgs} />)
-        expect(screen.getByTestId('status')).toHaveStyle({
-            backgroundColor: 'red'
-        })
-
-        rerender(<Default {...unknownArgs} />)
-        expect(screen.getByTestId('status')).toHaveStyle({
-            backgroundColor: 'yellow'
-        })
-    })
 })
