de65a4bcf24fa7d7e285cbe943b0e99f3c73abf6
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-20 18:48:14 -0300

    feat(ProfilePage): Add Location and Origin sections

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,18 +1,23 @@
 import { ProfileCardProps } from '../ProfileCard'
 import { PageContainer } from './styles'
 import ProfileHeader from '../ProfileHeader'
+import LocationSection, { LocationSectionProps } from '../LocationSection'
 
 export type ProfilePageProps = {
-    character: ProfileCardProps
+    character: ProfileCardProps,
+    location: LocationSectionProps,
+    origin: LocationSectionProps
 }
 
-const ProfilePage = ({ character }: ProfilePageProps) => {
+const ProfilePage = ({ character, location, origin }: ProfilePageProps) => {
     const { image, name, status, species } = character
     const profileHeaderProps = { image, name, status, species, showAs: 'page' as const }
 
     return (
         <PageContainer>
             <ProfileHeader {...profileHeaderProps} />
+            <LocationSection {...origin} />
+            <LocationSection {...location} />
         </PageContainer>
     )
 }
