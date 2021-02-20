9a2d64e8e11d249dc6f61c77a22ab6a23f2a610f
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-20 07:19:04 -0300

    fix(Profile): Typescripts errors on tests

--- src/components/Profile/test.tsx
+++ src/components/Profile/test.tsx
@@ -1,25 +1,31 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { Default } from './stories'
+import { ProfileProps } from '.'
 
 describe('<Profile />', () => {
     it('should render all props passed', () => {
-        render(<Default {...Default.args} />)
+        const props = Default.args as ProfileProps
+        render(<Default {...props} />)
 
-        expect(screen.getByText(Default.args.name)).toBeInTheDocument()
-        expect(screen.getByText(new RegExp(Default.args.status, "i"))).toBeInTheDocument()
-        expect(screen.getByText(new RegExp(Default.args.species, "i"))).toBeInTheDocument()
-        expect(screen.getByAltText(Default.args.name).src).toContain(Default.args.image)
-        expect(screen.getByText(new RegExp(Default.args.origin, "i"))).toBeInTheDocument()
-        expect(screen.getByText(new RegExp(Default.args.location, "i"))).toBeInTheDocument()
-        const episodesText = `Episodes it appeared in: ${Default.args?.number_of_episodes}`
+        expect(screen.getByText(props.name!)).toBeInTheDocument()
+        expect(screen.getByText(new RegExp(props.status!, "i"))).toBeInTheDocument()
+        expect(screen.getByText(new RegExp(props.species, "i"))).toBeInTheDocument()
+
+        const image = screen.getByAltText(props.name) as HTMLImageElement
+        expect(image.src).toContain(props.image)
+        expect(screen.getByText(new RegExp(props.origin, "i"))).toBeInTheDocument()
+        expect(screen.getByText(new RegExp(props.location, "i"))).toBeInTheDocument()
+
+        const episodesText = `Episodes it appeared in: ${props.numberOfEpisodes}`
         expect(screen.getByText(new RegExp(episodesText, "i"))).toBeInTheDocument()
     })
 
     it('should render the status circle color accordingly', () => {
-        const aliveArgs = { ...Default.args, status: 'Alive' }
-        const deadArgs = { ...Default.args, status: 'Dead' }
-        const unknownArgs = { ...Default.args, status: 'unknown' }
+        const props = Default.args as ProfileProps
+        const aliveArgs = { ...props, status: 'Alive' as ProfileProps['status'] }
+        const deadArgs = { ...props, status: 'Dead' as ProfileProps['status'] }
+        const unknownArgs = { ...props, status: 'unknown' as ProfileProps['status'] }
         
         const {rerender } = render(<Default {...aliveArgs} />)
         expect(screen.getByTestId('status')).toHaveStyle({
