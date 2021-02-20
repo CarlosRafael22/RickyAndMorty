74bbb539fe90cdcfaae7f32e758719ef8bba115f
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-20 19:11:03 -0300

    feat(pages/profile): Retrieve location from url and show

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,10 +1,14 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import ProfilePage from '../../src/components/ProfilePage'
+import { parseLocation } from '../../src/utils/parsers'
 
 const Page = () => {
     const { query: { id } } = useRouter()
     const [character, setCharacter] = useState<any | null>(null)
+    const [origin, setOrigin] = useState<any | null>(null)
+    // const [location, setLocation] = useState<any | null>(null)
+
     console.log('QUERY: ', id)
 
     useEffect(() => {
@@ -14,6 +18,18 @@ const Page = () => {
           const data = await response.json()
           console.log(data)
           setCharacter(data)
+
+          const originUrl = data.origin.url
+          const locationUrl = data.origin.url
+          console.log('AS URLS: ', originUrl, locationUrl)
+
+          const originResponse = await fetch(originUrl)
+          console.log(originResponse)
+          const originData = await originResponse.json()
+          console.log(originData)
+          const parsedOrigin = parseLocation(originData)
+          console.log(parsedOrigin)
+          setOrigin(parsedOrigin)
         }
     
         getData()
@@ -22,8 +38,8 @@ const Page = () => {
     return (
         <>
         {
-            character === null ? (<p>Loading</p>)
-            : <ProfilePage character={character} />
+            (character === null && origin === null) ? (<p>Loading</p>)
+            : <ProfilePage character={character} origin={origin} location={origin} />
         }
         </>
     )
