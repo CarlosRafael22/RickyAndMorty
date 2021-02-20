0d3274a71f3e04fc4f9ed2b44de630c51671837e
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-20 20:15:54 -0300

    style(ProfilePage): Change background and color

--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 export const PageContainer = styled.main`
-    width: 60%;
+    width: 40%;
     height: 100%;
     margin: 0 auto;
-    background-color: grey
+    background-color: hsla(0,0%,0%,0.8);
+    color: white;
 `
