010408b61d695b41f8933bd104f3146bb1bbeb80
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-20 18:23:55 -0300

    feat: Create LocationSection with initial story

---
+++ src/components/LocationSection/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const LocationSectionStyle = styled.div`
+    padding: 1rem;
+`
+
+export const LocationHeader = styled.h4`
+    font-size: 1.3rem;
+    margin: 0.5rem 0;
+`
+
+export const LocationInfoSectionStyle = styled.div`
+    display: grid;
+    grid-template-columns: auto auto;
+    font-size: 1.2rem;
+`
+
+export const LocationInfo = styled.p`
+    margin: 0.4rem;
+`
+
+export const InfoSpan = styled.span`
+    font-weight: 600;
+`
