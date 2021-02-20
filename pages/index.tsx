6741163453516e2cc94db956c8ab5254bf3b42f7
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-20 20:16:29 -0300

    refactor(pages): Use requestParsedCharacters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,14 @@
 import { useEffect, useState } from 'react'
 import { ProfileCardProps } from '../src/components/ProfileCard'
 import ProfilesSection from '../src/components/ProfilesSection'
+import { requestParsedCharacters } from '../src/utils/requests'
 
 const Home = () => {
   const [characters, setCharacters] = useState<ProfileCardProps[]>([])
 
-  const parseCharacters = (characters: any[]): ProfileCardProps[] => {
-    return characters.map(character => ({
-      id: character.id,
-      image: character.image,
-      name: character.name,
-      status: character.status,
-      species: character.species,
-      location: character.location,
-      origin: character.origin,
-      numberOfEpisodes: character.episode.length
-    }))
-  }
-
   useEffect(() => {
     const getData = async () => {
-      const response = await fetch('https://rickandmortyapi.com/api/character')
-      console.log(response)
-      const data = await response.json()
-      console.log(data)
-      const parsedCharacters = parseCharacters(data.results)
-      console.log(parsedCharacters.slice(0, 5))
+      const parsedCharacters = await requestParsedCharacters()
       setCharacters(parsedCharacters)
     }
 
