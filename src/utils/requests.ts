d72535e46f4b6f82ba7183f9856afb4d7f17f69b
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-21 14:54:40 -0300

    feat(requests): Create requestParsedEpisodes

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,4 +1,4 @@
-import { parseLocation, parseCharacters } from './parsers'
+import { parseLocation, parseCharacters, parseEpisodes } from './parsers'
 
 export const requestParsedLocation = async (url: string) => {
     const originResponse = await fetch(url)
@@ -12,4 +12,14 @@ export const requestParsedCharacters = async () => {
     const data = await response.json()
     const parsedCharacters = parseCharacters(data.results)
     return parsedCharacters
+}
+
+export const requestParsedEpisodes = async (episodesIds: number[]) => {
+    const response = await fetch(`https://rickandmortyapi.com/api/episode/${episodesIds}`)
+    const data = await response.json()
+    // If episodesIds only has one id then the return will be an object na not array
+    // We put the object in the array for pattern
+    const episodes = Array.isArray(data) ? data : [data]
+    const parsedEpisodes = parseEpisodes(episodes)
+    return parsedEpisodes
   }
