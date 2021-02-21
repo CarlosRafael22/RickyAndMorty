ac23c9f27f0441ff262e71237d34281fc400bf3d
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-21 13:44:51 -0300

    fix(ProfileCard): GetByText for origin and location on test

--- src/components/ProfileCard/test.tsx
+++ src/components/ProfileCard/test.tsx
@@ -14,8 +14,8 @@ describe('<Profile />', () => {
 
         const image = screen.getByAltText(props.name) as HTMLImageElement
         expect(image.src).toContain(props.image)
-        expect(screen.getByText(new RegExp(props.origin, "i"))).toBeInTheDocument()
-        expect(screen.getByText(new RegExp(props.location, "i"))).toBeInTheDocument()
+        expect(screen.getByText(new RegExp(props.origin.name, "i"))).toBeInTheDocument()
+        expect(screen.getByText(new RegExp(props.location.name, "i"))).toBeInTheDocument()
 
         const episodesText = `Episodes it appeared in: ${props.numberOfEpisodes}`
         expect(screen.getByText(new RegExp(episodesText, "i"))).toBeInTheDocument()
